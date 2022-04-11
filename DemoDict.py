c78cca4ec3e18cbe85200024c33138cf62af2c18
HYEONGRAESUH <destiny8712@naver.com>
2022-04-11 16:55:34 +0900

    연습

--- DemoDict.py
+++ DemoDict.py
@@ -13,3 +13,13 @@ print(b)
 color ={"apple":"red","banana":"yellow"}
 print(type(color))
 print(color["apple"])
+
+device = {"아이폰":5, "아이패드":10, "윈도우":20}
+print(device["아이폰"])
+
+device["맥북"]=15
+
+device["아이폰"]=6
+
+for item in device.items():
+    print(item)
