9399a7e252474ab22fdb08b73d67dd11ddf71aa8
HYEONGRAESUH <destiny8712@naver.com>
2022-04-12 09:39:02 +0900

    예제를 수정

--- DemoDict2.py
+++ DemoDict2.py
@@ -1,7 +1,19 @@
 #DemoDict2.py
 
+from re import A
+
+
 phone={"kim":"1111","lee":"2222"}
 print("kim" in phone)
 print("kang" not in phone)
 p=phone
 print(id(phone),id(p))
+
+
+#깊은복사
+a = [100,200,300]
+b = a[:]
+a[0]=101
+print(a)
+print(b)
+print(id(a),id(b))
