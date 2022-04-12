8615a2f45194ee9cc5e87462bf49ab8fbaca13b7
HYEONGRAESUH <destiny8712@naver.com>
2022-04-12 10:34:55 +0900

    구구단

--- DemoLoop.py
+++ DemoLoop.py
@@ -6,3 +6,19 @@ while value > 0:
     value -= 1
 
 print("전체코드실행종료")
+
+#반복구무능ㄹ 원하는만큼
+
+for i in range(5):
+    print(i)
+
+lst = ["apple", 100, 3.14]
+for item in lst:
+    print(item, type(item))
+
+
+#구구단 출력
+for i in [2,3,4,5,6]:
+    print("---{0}단---".format(i))
+    for j in [1,2,3,4,5,6,7,8,9]:
+        print("{0}*{1}={2}".format(i,j,i*j))
